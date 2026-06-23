9046c8f02cfa9c34463673f938b3008407a93239
agaetolive-alt <agaetolive@gmail.com>
2026-06-23 07:52:20 +0530

    fix main class

--- src/main/java/walkys/optimizer/WalksyCrystalOptimizer.java
+++ src/main/java/walkys/optimizer/WalksyCrystalOptimizer.java
@@ -1,1 +1,20 @@
+package walksy.optimizer;
 
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+
+public class WalkysCrystalOptimizer implements ClientModInitializer {
+    private static Optimizer optimizer;
+    
+    @Override
+    public void onInitializeClient() {
+        optimizer = new Optimizer();
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            optimizer.tick();
+        });
+    }
+    
+    public static Optimizer getOptimizer() {
+        return optimizer;
+    }
+}
